4aa6c3472e1dc2c3d87383fe44fc80516e265121
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-30 15:15:26 +0200

    Update loader.vue

--- airbean/src/views/loader.vue
+++ airbean/src/views/loader.vue
@@ -1,25 +1,25 @@
 <template>
-    <article class="loading-container">
-        <img src="../assets/graphics/loader.png" alt="loader">
-    </article>
+  <article class="loading-container">
+    <img src="../assets/graphics/loader.png" alt="loader" />
+  </article>
 </template>
 
 <script>
 export default {
-  created () {
+  created() {
     setTimeout(() => this.$router.push('/status'), 5000)
   }
 }
 </script>
 
 <style scoped>
-    .loading-container {
-        width: 100vw;
-        height: 100vh;
-        margin: auto;
-        background-color: #2F2926;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.loading-container {
+  width: 100vw;
+  height: 100vh;
+  margin: auto;
+  background-color: #2f2926;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
